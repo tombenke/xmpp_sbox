426e69441b7b7d88d16eb065cbea6df983645382
Gellert Mezey <gellert.mezey@lhsystems.com>
2015-09-07 16:13:18 +0200

    connect function

--- test/connect.js
+++ test/connect.js
@@ -1,25 +1,26 @@
 #!/usr/bin/env node
 'use strict';
 
-var xmpp = require('node-xmpp-client');
-var ltx = require('ltx');
+var XmppClient = require('../lib/XmppClient');
+var should = require('should');
 
-var client = new xmpp({ jid: 'tombenke@u204400m1', password: 'tombenke' });
 
-client.on('online', function() {
-    console.log('[online]');
-    client.send(new ltx.Element('presence', { })
-      .c('show').t('chat').up()
-      .c('status').t('Happily echoing your <message/> stanzas')
-    );
+describe('XmppClient', function () {
+    it('should connect to server', function (done) {
+
+        var client = new XmppClient({
+            jid:      'user01@clc',
+            password: 'pass01',
+            host:     'localhost'
+        });
+
+        client.on('online', function (data) {
+            data.jid.should.have.property('local', 'user01');
+            data.jid.should.have.property('domain', 'clc');
+            data.jid.should.have.property('resource');
+            data.jid.should.have.property('user', 'user01');
+            done();
+        });
+
+    });
 });
-
-client.on('stanza', function(stanza) {
-    console.log('[stanza]: ', stanza.toString());
-});
-
-client.on('error', function(e) {
-    console.error(e);
-});
-
-client.end();
