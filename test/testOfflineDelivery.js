99598e1d3ee039301409045b80db4573b02aba74
bersilius <adam.bereczk@gmail.com>
2015-10-06 11:05:31 +0200

    Add client connect and disconnect workflow

--- test/testOfflineDelivery.js
+++ test/testOfflineDelivery.js
@@ -36,6 +36,61 @@ describe('stanza.io offline message delivery test', function () {
 
         async.series([
 
+            function (cb) {
+                han = XMPP.createClient({
+                    jid: users.Han_Solo.jid,
+                    password: users.Han_Solo.password,
+                    wsURL: 'ws://localhost:5280/websocket',
+                    transport: 'websocket'
+                });
+
+                cb();
+            },
+
+            function (cb) {
+                if (debug === true) {
+                    han.on('*', function (name, data) {
+                        log(users.Han_Solo.debugOptions, name, data);
+                    });
+
+                    }
+                cb();
+            },
+
+            function (cb) {
+                han.once('session:started', function () {
+                    cb();
+                });
+
+                han.connect();
+            },
+
+            function (cb) {
+                han.getRoster(function (err, resp) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        should(resp).have.property('roster');
+                        should(resp).have.property('from')
+                        resp.from.full.should.equal(users.Han_Solo.jid);
+                        log(users.Han_Solo.debugOptions, resp);
+                    }
+                    cb();
+                });
+            },
+
+            function (cb) {
+                han.sendPresence({
+                    
+                });
+                cb();
+            },
+
+            function (cb) {
+                han.disconnect();
+                cb();
+            },
+
             function () {
                 done();
             }
