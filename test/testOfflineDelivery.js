346180340bc4b6078a5e171568b04c8361c2f504
bersilius <adam.bereczk@gmail.com>
2015-10-06 13:47:49 +0200

    Merge offline tests

--- test/testOfflineDelivery.js
+++ test/testOfflineDelivery.js
@@ -29,7 +29,7 @@ var users = {
 
 describe('stanza.io offline message delivery test', function () {
 
-    var han;
+    var han, chewie;
 
     it('A client should connect and send a message to an offline client', function (done) {
         this.timeout(10000);
@@ -94,6 +94,73 @@ describe('stanza.io offline message delivery test', function () {
                 cb();
             },
 
+            function (cb) {
+                han.on('disconnected', function () {
+                    cb();
+                })
+                han.disconnect();  
+            },
+
+            function (cb) {
+                chewie = XMPP.createClient({
+                    jid: users.Chewie.jid,
+                    password: users.Chewie.password,
+                    wsURL: 'ws://localhost:5280/websocket',
+                    transport: 'websocket'
+                });
+
+                cb();
+            },
+
+            function (cb) {
+                if (debug === true) {
+                    chewie.on('*', function (name, data) {
+                        log(users.Chewie.debugOptions, name, data);
+                    });
+                }
+                cb();
+            },
+
+            function (cb) {
+                chewie.on('message', function (msg) {
+                    should(msg).have.property('to');
+                    msg.to.full.should.equal(users.Chewie.jid);
+                    should(msg).have.property('body', 'Chewie, are you there?');
+                    should(msg).have.property('delay');
+                    log(users.Han_Solo.debugOptions, msg);
+                });
+                cb();
+            },
+
+            function (cb) {
+                chewie.once('session:started', function () {
+                    cb();
+                });
+
+                chewie.connect();
+            },
+
+            function (cb) {
+                chewie.getRoster(function (err, resp) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        should(resp).have.property('roster');
+                        should(resp).have.property('from')
+                        resp.from.full.should.equal(users.Chewie.jid);
+                        log(users.Chewie.debugOptions, resp);
+                    }
+                    cb();
+                });
+            },
+
+            function (cb) {
+                chewie.sendPresence({
+                    
+                });
+                cb();
+            },
+
             function () {
                 done();
             }
@@ -102,6 +169,6 @@ describe('stanza.io offline message delivery test', function () {
     });
 
     after(function() {
-        han.disconnect();
+        chewie.disconnect();
     });
 });
