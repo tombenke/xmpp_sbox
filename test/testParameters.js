0761d20e857f6c1ce1dc037cabf927a6e9e0f0ee
bersilius <adam.bereczk@gmail.com>
2015-10-15 10:12:50 +0200

    Add new file 'testParameters.js', containing default test params

---
+++ test/testParameters.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+'use strict';
+
+var logger = require('../libs/log');
+
+module.exports.rebels = function() {
+	return {
+        rebelsAdmin: {
+            jid:        'rebels.admin@rebels',
+            password:   'pass123',
+            host:       'localhost',
+            log:        logger.createLogger('admin', { keysColor: 'red'}),
+        },
+        han: {
+            jid:        'han.solo@rebels',
+            password:   'pass123',
+            host:       'localhost',
+            log:        logger.createLogger('han.solo', { keysColor: 'cyan' }),
+        },
+        chewie: {
+            jid:        'chewbacca@rebels',
+            password:   'pass123',
+            host:       'localhost',
+            log:         logger.createLogger('chewbacca', { keysColor: 'yellow' }),
+        },
+        r2d2: {
+            jid:        'r2d2@rebels',
+            password:   'pass123',
+            host:       'localhost',
+            log:        logger.createLogger('r2d2', { keysColor: 'green' })
+        },
+        leia: {
+            jid:        'leia.organa@rebels',
+            password:   'pass123',
+            host:       'localhost',
+            log:        logger.createLogger('leia', { keysColor: 'magenta' })
+        }
+    };
+};
+
+module.exports.empire = function() {
+    return {
+        empireAdmin: {
+            jid:        'empire.admin@empire',
+            password:   'pass123',
+            host:       'localhost',
+            log:        logger.createLogger('admin', { keysColor: 'red'}),
+        },
+        vader: {
+            jid:        'darth.vader@empire',
+            password:   'pass123',
+            host:       'localhost',
+            log:        logger.createLogger('darth.vader', { keysColor: 'cyan' }),
+        },
+        emperor: {
+            jid:        'emperor@empire',
+            password:   'pass123',
+            host:       'localhost',
+            log:        logger.createLogger('emperor', { keysColor: 'yellow' }),
+        },
+        tarkin: {
+            jid:        'wilhuf.tarkin@empire',
+            password:   'pass123',
+            host:       'localhost',
+            log:        logger.createLogger('tarkin', { keysColor: 'green' })
+        },
+        thrawn: {
+            jid:        'thrawn@empire',
+            password:   'pass123',
+            host:       'localhost',
+            log:        logger.createLogger('tarkin', { keysColor: 'green' })
+        }
+    };
+};
