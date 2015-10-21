2cbf114b6b5ba742c0b963144a00f2667ea7323f
Gellert Mezey <gellert.mezey@lhsystems.com>
2015-10-21 10:46:11 +0200

    remove prettyjson from dependencies

--- libs/log.js
+++ libs/log.js
@@ -1,24 +1,6 @@
-var prettyjson = require('prettyjson');
 var colors = require('colors/safe');
 var YAML = require('yamljs');
 
-
-module.exports = function log() {
-
-    var logEntry;
-
-    for (var i = 1; i < arguments.length; i++) {
-
-        if (i === 1) {
-            logEntry = {logEntry: JSON.stringify(arguments[i])};
-            console.log(prettyjson.render(logEntry, arguments[0]));
-        } else {
-            console.log(arguments[i]);
-        }
-
-    };
-    console.log('----------------------------');
-};
 
 module.exports.createLogger = function (name, opts) {
     return function (evt, msg) {
